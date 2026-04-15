dfce1385bdce80ef5fe371dcf8f96509e9eff0f4
NALNOP . <wizardb67@gmail.com>
2026-04-16 05:43:46 +0700

    fix: add missing intl package and fix broken imports in notification feature

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
